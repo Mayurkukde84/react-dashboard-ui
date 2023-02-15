802f64974878e1cf64ef00cd84ca9ce8e588b608
mayur <mayurkukde84@gmail.com>
2023-02-15 19:38:36 +0530

    complete leftside

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import './dashboard.css'
+import React from "react";
+import "./dashboard.css";
+
 const Dashboard = () => {
   return (
-    <div className='dashboard-container'>Dashboard</div>
-  )
-}
+    <div className="dashboard-container">
+      <div className="title">
+        <h3>Dashboard</h3>
+      </div>
+      <div className="card-item">
+      <div className="top-heading">
+      <h5>Total Revenue</h5>
+      </div>
+        
+        <div className="price">
+          <h4>$11,354.00</h4>
+          <p>+6.32%</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -1,51 +1,82 @@
-import React from 'react'
-import './leftbar.css'
-import {RxDashboard} from 'react-icons/rx';
-import {BsFileBarGraph,BsPeople} from 'react-icons/bs'
-import {BiMessageAltDetail} from 'react-icons/bi'
-import {TbChartRadar} from 'react-icons/tb'
-import {CgPerformance} from 'react-icons/cg'
-import {AiOutlineHistory} from 'react-icons/ai'
+import React from "react";
+import "./leftbar.css";
+import { RxDashboard } from "react-icons/rx";
+import { BsFileBarGraph, BsPeople } from "react-icons/bs";
+import { BiMessageAltDetail } from "react-icons/bi";
+import { TbChartRadar } from "react-icons/tb";
+import { CgPerformance } from "react-icons/cg";
+import { AiOutlineHistory } from "react-icons/ai";
+import {FiLogOut} from 'react-icons/fi'
 const Leftbar = () => {
   return (
-    <div className='left-Container'>
-    <div className="logo">
-    <h3>Horizon</h3>
+    <div className="left-Container">
+      <div className="logo">
+        <h3>Horizon</h3>
+      </div>
+      <div className="user-profile">
+        <div className="user-img">
+          <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="img" />
+        </div>
+        <h5>Daniel Kevin</h5>
+        <p>danielkevin@gmail.com</p>
+      </div>
+      <div className="list-services">
+        <div className="icon-list dashboard">
+          <p>
+            <span className="actives"><RxDashboard /></span>
+            Dashbard
+          </p>
+        </div>
+        <div className="icon-list">
+          <p>
+            <span><BsFileBarGraph /></span> 
+            Statistic
+          </p>
+        </div>
+        <div className="icon-list">
+          <p>
+            <span><BsPeople /></span> 
+            Audience
+          </p>
+        </div>
+        <div className="icon-list">
+          <p>
+          <span><BiMessageAltDetail /></span>
+            
+            Massage
+          </p>
+        </div>
+        <div className="icon-list">
+          <p>
+          <span> <TbChartRadar /></span>
+           
+            Campaigns
+          </p>
+        </div>
+        <div className="icon-list">
+          <p>
+          <span><CgPerformance /></span>
+            
+            Performance
+          </p>
+        </div>
+        <div className="icon-list">
+          <p>
+          <span><AiOutlineHistory /></span>
+            
+            History
+          </p>
+        </div>
+        <div className="icon-list logout">
+          <p>
+          <span><FiLogOut /></span>
+            
+            LogOut
+          </p>
+        </div>
+      </div>
     </div>
-    <div className="user-profile">
-      <div className="user-img">
-        <img src='https://randomuser.me/api/portraits/men/11.jpg' alt='img' />
-      </div>
-      <h5>Daniel Kevin</h5>
-      <p>danielkevin@gmail.com</p>
-    </div>
-    <div className="list-services">
-      <div className="dashboard">
-   
-        <p><RxDashboard />Dashbard</p>
-      </div>
-      <div className="statistics">
-        <p><BsFileBarGraph/>Statistic</p>
-      </div>
-      <div className="audience">
-        <p><BsPeople/>Audience</p>
-      </div>
-      <div className="dashboard">
-        <p><BiMessageAltDetail/>Massage</p>
-      </div>
-      <div className="dashboard">
-        <p><TbChartRadar/>Campaigns</p>
-      </div>
-      <div className="dashboard">
-        <p><CgPerformance/>Performance</p>
-      </div>
-      <div className="dashboard">
-        <p><AiOutlineHistory/>History</p>
-      </div>
-    </div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default Leftbar
+export default Leftbar;
