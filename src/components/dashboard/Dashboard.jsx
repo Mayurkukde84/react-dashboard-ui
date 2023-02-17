ef416c148409cadb38af3df9bc8e6353e028ddbd
mayur <mayurkukde84@gmail.com>
2023-02-17 19:57:03 +0530

    added card

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -16,71 +16,72 @@ const Dashboard = () => {
     <>
       <div className="dashboard-container">
         <div className="dashbord-title">
-          <h4>Dashboard</h4>
+          <h3>Dashboard</h3>
         </div>
-      <div className="card-container">
+        
+        <div className="card-container">
+         
 
-      
-        <div className="card">
-          <div className="card-item">
-            <div className="top-heading">
-              <h5>Total Revenue</h5>
-              <div className="circle">
-                <div style={{ width: 35 }}>
-                  <CircularProgressbar
-                    value={percentage}
-                    text={`$`}
-                  />
+          <div className="card ">
+            <div className="card-item active-card">
+              <div className="top-heading">
+                <h5>Total Revenue</h5>
+                <div className="circle">
+                  <div style={{ width: 35 }}>
+                    <CircularProgressbar
+                      value={percentage}
+                      text={`${percentage}%`}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="price">
-              <h4>$11,354.00</h4>
-              <p>+6.32%</p>
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-item">
-            <div className="top-heading">
-              <h5>Total Revenue</h5>
-              <div className="circle">
-                <div style={{ width: 35 }}>
-                  <CircularProgressbar
-                    value={percentage}
-                    text={`${percentage}%`}
-                  />
-                </div>
+              <div className="price">
+                <h4>$11,354.00</h4>
+                <p>+6.32%</p>
               </div>
             </div>
-
-            <div className="price">
-              <h4>$11,354.00</h4>
-              <p>+6.32%</p>
-            </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-item">
-            <div className="top-heading">
-              <h5>Total Revenue</h5>
-              <div className="circle">
-                <div style={{ width: 35 }}>
-                  <CircularProgressbar
-                    value={percentage}
-                    text={`${percentage}%`}
-                  />
+          <div className="card">
+            <div className="card-item">
+              <div className="top-heading">
+                <h5>Total Customer</h5>
+                <div className="circle">
+                  <div style={{ width: 35 }}>
+                    <CircularProgressbar
+                      value={50}
+                      text={`$50%`}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="price">
-              <h4>$11,354.00</h4>
-              <p>+6.32%</p>
+              <div className="price">
+                <h4>45,439</h4>
+                <p>+12.56%</p>
+              </div>
             </div>
           </div>
-        </div>
+          <div className="card">
+            <div className="card-item">
+              <div className="top-heading">
+                <h5>Total Profit</h5>
+                <div className="circle">
+                  <div style={{ width: 35 }}>
+                    <CircularProgressbar
+                      value={40}
+                      text={`$40%`}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="price">
+                <h4>$8,354.00</h4>
+                <p>+3.12%</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="spending-statistics">
           <h1>spending statistics</h1>
