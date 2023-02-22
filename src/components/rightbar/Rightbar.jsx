f8e6ed92981340bf25e9a9ff48582497f786b8a0
mayur <mayurkukde84@gmail.com>
2023-02-22 21:41:30 +0530

    add right side bar data

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './rightbar.css'
+import React from "react";
+import "./rightbar.css";
 const Rightbar = () => {
   return (
-    <div className='right-container'>
+    <div className="right-container">
       <nav>
-        <input
-        placeholder='Search...'
-         />
+        <input placeholder="Search..." />
       </nav>
+      <div className="map">
+        <h3>Customer by country</h3>
+        <h3>...</h3>
+        <p>Map</p>
+      </div>
+      <div className="recentSales">
+        <div className="recent-viewAll">
+          <h3>Recent Sales</h3>
+          <p>View All</p>
+        </div>
+        <p>profiles</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Rightbar
+export default Rightbar;
